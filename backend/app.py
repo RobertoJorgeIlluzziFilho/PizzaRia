e6bf38b8f45cfc7df8c74f738e6561b4cf1e8d1f
Illuzzi <roberward933@gmail.com>
2026-05-20 17:41:56 -0300

    Simulação de validação e tratamento de dados com o JSON

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,47 @@
-from flask import Flask
+from flask import Flask, json, jsonify, request
 
+# Objeto app
 app = Flask(__name__)
 
 # Decorator pra rota padrão 
 @app.route("/")
-def hello_world():
-    return "<p> Olá, mundo! <p>"
+def main():
+    return "<p> Bem vindo a página inicial! <p>"
+
+# Apenas POST, pois no react moderno nem existe GET /login
+# React renderiza a tela, então nosso flask vira somente API 
+@app.route("/login", methods=["POST"])
+def login():
+
+    # recebe a resposta em JSON
+    dados = request.json
+
+    # recebe o atributo inserido
+    email = dados['email']
+    password = dados['password']
+
+    # validação
+    if(valid_login(email, password)):
+        return log_the_user(email)
+
 
 # Decorator pra rota 'robots.txt'
 @app.route("/robots.txt")
 def robot():
     return 'key.txt'
 
-
+#######################
+#       FUNÇÕES
+#######################
+def valid_login(email, password):
+    # faça a leitura do nosso "Banco de dados"
+    with open("/data/usuarios.json", "r") as arquivo:
+        users = json.load(arquivo)
+    for usuario in users:
+        if(usuario['email'] == email and usuario['password'] == password):
+            pass 
+        
+def log_the_user(username):
+    pass # conta do usuário 
 if __name__ == '__main__':
     app.run(debug=False)
