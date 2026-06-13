d56e2ce9db624bbd4a3f07bef680150676515166
Illuzzi <roberward933@gmail.com>
2026-06-12 22:38:36 -0300

    adicionado: escopo do(s) pedido(s)

--- backend/routes/pedidos.py
+++ backend/routes/pedidos.py
@@ -7,3 +7,12 @@ pedidos_bp = Blueprint('pedidos', __name__)
 @pedidos_bp.route('/pedidos', methods=["GET"])
 def listar_pedidos():
     return "pedidos sendo listados"
+
+# Vou precisar criar o pedido no usuário logado atualmente
+@pedidos_bp.route('/pedidos', methods=["POST"])
+def criar_pedidos():
+    return
+@pedidos_bp.route('/pedidos', methods=["DELETE"])
+def remover_pedidos():
+    return
+
