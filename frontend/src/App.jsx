e1fd682abbc8d3889cb2b340b21e1598e8858dbc
Illuzzi <roberward933@gmail.com>
2026-07-02 17:36:56 -0300

    proteger rotas privadas com o auth guard/ProtectedRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,76 @@
+// O App é o responsável por definir todas as rotas da aplicação
 
-// o App.jsx é o gerenciador principal, ele decide:
-// Qual página aparece?
-// Qual rota aparece?
-// Qual layout aparece?
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import "./styles/forms.css";
 
-
-// Rotas no REACT
-import Home from './pages/Home'  // página após o usuário se autenticar
-import Login from './pages/Login' // vai ser nossa "home" inicial
-// import Cadastro from './pages/Cadastro'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Cadastro from './pages/Cadastro'
+import Cardapio from './pages/Cardapio'
+import MeusPedidos from './pages/MeusPedidos'
+import NovoPedido from './pages/NovoPedido'
+import Perfil from './pages/Perfil'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-      <Login />
+    <div>
+      <BrowserRouter>
+        <Routes>
+
+          {/* Rotas públicas */}
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+
+          {/* Rotas que exigem login */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/cardapio"
+            element={
+              <ProtectedRoute>
+                <Cardapio />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/meus-pedidos"
+            element={
+              <ProtectedRoute>
+                <MeusPedidos />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/novo-pedido"
+            element={
+              <ProtectedRoute>
+                <NovoPedido />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/perfil"
+            element={
+              <ProtectedRoute>
+                <Perfil />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
-// exportando nosso app pra fora
 export default App
